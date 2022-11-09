a2451aed45f9eb1ee67d7962b54d105c3a053b4a
Red-X12 <2036917604@qq.com>
2022-11-09 21:35:26 +0800

    给Student添加age name 属性

--- TestModule/src/com/xuan/Student.java
+++ TestModule/src/com/xuan/Student.java
@@ -3,11 +3,10 @@ package com.xuan;
 /**
  * @version 1.0
  * @Author Xuan
- * @Date 2022/11/9-11-09-20:09
+ * @Date 2022/11/9-11-09-21:34
  * @Description com.xuan
  **/
 public class Student {
     private int age;
     private String name;
-
 }
